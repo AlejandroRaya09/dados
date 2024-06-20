97cbc8edd40596e6c243958af71383ea98713236
Alejandro Raya <alejandroraya09@hotmail.com>
2024-06-20 00:55:35 -0600

    Dados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'dados';
+  dadoIzquierdo = '../assets/img/dice1.png';
+  dadoDerecho = '../assets/img/dice5.png';
+
+  numero1: number = 1;
+  numero2: number = 2;
+
+  tirarDados() {
+    this.numero1 = Math.round(Math.random() * 5) + 1;
+    this.numero2 = Math.round(Math.random() * 5) + 1;
+
+
+    this.dadoIzquierdo = '../assets/img/dice' + this.numero1 +'.png'
+    this.dadoDerecho = '../assets/img/dice' + this.numero2 +'.png'
+  }
+
+
 }
